5a4e0e6f50c2604b8cb1ddfb69ba7ce0877eb853
Isaac Garcia <garciaalejoisaac@gmail.com>
2024-11-03 19:14:45 -0500

    Avance 3/11/2024

---
+++ Hello.py
@@ -0,0 +1,14 @@
+print("Hello my dear world")
+
+num1 = int(input("Ingrese un numero: "))
+num2 = int(input("Ingrese otro numero: "))
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicar = num1 * num2
+dividir = num1 / num2
+
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La multiplicación es : ", multiplicar)
+print("La división es : ", dividir)
---
+++ clase#2/clase-#-2.py
@@ -0,0 +1,12 @@
+saludo = "Hola"
+nombre = " Isaac"
+edad = "20 años"
+
+print(saludo + nombre)
+print("Tienes " + edad)
+
+
+# print("Te doy la bienvenida al Coding Playground")
+# print("Mi primer print")
+# print("30")
+
